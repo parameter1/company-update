08085948cbd4cec691fcb1bb256f52f391b3ccc5
Josh Worden <solocommand@gmail.com>
2019-12-03 09:14:16 -0600

    Move config up a level

--- services/app/app/routes/display.js
+++ services/app/app/routes/display.js
@@ -1,27 +1,4 @@
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
-import { queryManager } from 'ember-apollo-client';
-
-import query from 'cuf/gql/queries/config';
 
 export default Route.extend({
-  config: inject(),
-  apollo: queryManager(),
-
-  model() {
-    return this.apollo.watchQuery({ query, fetchPolicy: 'cache-and-network' }, 'companyUpdateConfig');
-  },
-  afterModel({
-    domain,
-    logo,
-    ids,
-    isPmmi,
-  }) {
-    this.config.load({
-      domain,
-      logo,
-      ids,
-      isPmmi,
-    });
-  },
 });
