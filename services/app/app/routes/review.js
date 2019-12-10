560854f733408313aa4f72aa233b09e1725a419e
Josh Worden <solocommand@gmail.com>
2019-12-10 13:01:52 -0600

    Fix bug when transitioning between loaded models

--- services/app/app/routes/review.js
+++ services/app/app/routes/review.js
@@ -10,13 +10,12 @@ export default Route.extend(AuthenticatedRouteMixin, {
   async model({ id }) {
     const variables = { id };
     const submission = await this.apollo.query({ query, variables }, 'companyUpdateSubmission');
+    if (!submission.parsed) {
+      submission.payload = JSON.parse(submission.payload);
+      submission.parsed = true;
+    }
     const { hash } = submission;
     const company = await this.apollo.query({ query: companyQuery, variables: { input: { hash } } }, 'contentHash');
     return { company, submission };
   },
-
-  afterModel(model) {
-    model.submission.payload = JSON.parse(model.submission.payload);
-  },
-
 });
