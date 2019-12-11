21d14b56d1aad98bd9afa066883c467f40302a30
Josh Worden <solocommand@gmail.com>
2019-12-10 18:23:54 -0600

    Cleanup

--- services/app/app/controllers/portal/company.js
+++ services/app/app/controllers/portal/company.js
@@ -2,25 +2,10 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
-  // Merges contacts into a single array
-  contacts: computed('model.{salesContacts,listingContacts,publicContacts}.[]', function() {
-    const contacts = [];
-    ['publicContacts', 'listingContacts', 'salesContacts'].forEach((type) => {
-      const check = this.get(`model.${type}`);
-      if (check && Object.prototype.hasOwnProperty.call(check, 'edges')) {
-        check.edges.forEach(({ node }) => contacts.push({ type, ...node }));
-      }
-    })
-    return contacts;
-  }),
 
   isModalOpen: false,
 
   logo: computed.reads('model.primaryImage.src'),
-
-  title: computed('model', function() {
-    return `${this.get('model.name')}`;
-  }),
 
   actions: {
     showModal() {
