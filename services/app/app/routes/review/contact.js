ff125a2905558ab7ebfdd75ba2cae52610daa9a4
Josh Worden <solocommand@gmail.com>
2019-12-16 11:27:14 -0600

    contact diff updates

--- services/app/app/routes/review/contact.js
+++ services/app/app/routes/review/contact.js
@@ -9,35 +9,42 @@ export default Route.extend({
   async model() {
     const { submission, company: { hash } } = this.modelFor('review');
     const company = await this.apollo.query({ query: companyQuery, variables: { input: { hash } } }, 'contentHash');
-    const { payload: { contacts: updated } } = submission;
+    const { payload: { add, remove, update } } = submission;
 
     const original = get(company, 'publicContacts.edges').map(({ node }) => {
       const { id, firstName, lastName, title, primaryImage } = node;
       return { id, firstName, lastName, title, primaryImage };
     });
 
-    const payload = {
-      enabled: true,
-      fields: {
-        firstName: true,
-        lastName: true,
-        primaryImage: true,
-        title: true,
-      },
-    };
-
     const contacts = original.reduce((arr, contact) => {
       const { id } = contact;
-      const found = updated.find(({ id: uid }) => uid === id);
-      if (found) {
-        updated.removeObject(found);
-        return [ ...arr, { original: contact, updated: found, payload: { ...payload } }];
+      const updated = update.find((u) => u.id === id);
+      if (updated) {
+        return [
+          ...arr,
+          {
+            original: contact,
+            updated: { ...contact, ...updated },
+            payload: { enabled: true, updated: true, fields: { ...updated } },
+          },
+        ];
       }
-      return [ ...arr, { original: contact, payload: { ...payload, removed: true } } ];
+      if (remove.includes(id)) {
+        return [
+          ...arr,
+          {
+            original: contact,
+            payload: { enabled: true, removed: true },
+          },
+        ];
+      }
+      return [ ...arr, { original: contact, payload: { enabled: false } } ];
     }, []);
-    updated.forEach((contact) => contacts.push({ updated: contact, payload: { ...payload, added: true } }));
+    add.forEach((contact) => contacts.push({
+      updated: contact,
+      payload: { enabled: true, added: true, fields: contact },
+    }));
     return { submission, company, contacts };
   },
-
 
 });
