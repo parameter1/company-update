9586f1d0550dc2d067e0b644aa70290f9b0103d3
Josh Worden <solocommand@gmail.com>
2019-12-16 10:18:29 -0600

    Compute payload from field state

--- services/app/app/components/contact-card.js
+++ services/app/app/components/contact-card.js
@@ -2,13 +2,39 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['col-md-6 col-12'],
+  contact: null,
+  _contact: null,
+
+  init() {
+    this._super(...arguments);
+    const {
+      firstName,
+      lastName,
+      title,
+      primaryImage,
+    } = this.get('contact');
+    this.set('_contact', {
+      firstName,
+      lastName,
+      title,
+      ...(primaryImage && { primaryImage: { src: primaryImage.src } }),
+    });
+  },
 
   actions: {
     remove() {
       this.onRemove();
     },
+    onChange(field, value) {
+      if (this.get(`contact.${field}`) === this.get(`_contact.${field}`)) {
+        this.onReset(field);
+      } else {
+        this.onUpdate(field, value);
+      }
+    },
     onUpload(src) {
-      this.set('contact.primaryImage', { src });
+      this.set('_contact.primaryImage', { src });
+      this.onUpdate('primaryImage', { src });
     }
   },
 });
--- services/app/app/controllers/portal/contacts.js
+++ services/app/app/controllers/portal/contacts.js
@@ -1,17 +1,72 @@
 import Controller from '@ember/controller';
+import { computed, set } from '@ember/object';
 
 export default Controller.extend({
   isModalOpen: false,
+
+  contacts: computed('model.[]', function() {
+    return this.get('model.publicContacts.edges').map(({ node }) => {
+      const { id, firstName, lastName, title, primaryImage } = node;
+      return { id, firstName, lastName, title, primaryImage };
+    });
+  }),
+
+  isSubmitDisabled: computed.not('isModified'),
+  isModified: computed('payload.{add.length,remove.length,update.length}', function() {
+    const adds = this.get('payload.add.length');
+    const removes = this.get('payload.remove.length');
+    const updates = this.get('payload.update.length');
+    return adds > 0 || removes > 0 || updates > 0;
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.set('payload', {
+      add: [],
+      remove: [],
+      update: [],
+    });
+  },
+
+  getUpdate(id) {
+    const found = this.get('payload.update').find((c) => c.id === id);
+    if (!found) {
+      const newObj = { id };
+      this.get('payload.update').pushObject(newObj);
+      return newObj;
+    }
+    return found;
+  },
 
   actions: {
     showModal() {
       this.set('isModalOpen', true);
     },
     add() {
-      this.model.pushObject({});
+      const contact = {};
+      this.contacts.pushObject(contact);
+      this.payload.add.pushObject(contact);
     },
     remove(contact) {
-      this.model.removeObject(contact);
+      this.contacts.removeObject(contact);
+      if (contact.id) {
+        this.payload.remove.pushObject(contact.id);
+      } else {
+        this.payload.add.removeObject(contact);
+      }
+    },
+    update({ id }, field, value) {
+      if (id) {
+        const entry = this.getUpdate(id);
+        set(entry, field, value);
+      }
+    },
+    reset({ id }, field) {
+      if (id) {
+        const entry = this.getUpdate(id);
+        delete entry[field];
+        if (Object.keys(entry).length === 1) this.payload.update.removeObject(entry);
+      }
     },
     transitionToPortal() {
       this.transitionToRoute('portal');
