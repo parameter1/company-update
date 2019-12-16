7149a51b5f6e7a4c3a9f986c6fa5c75b9c99a599
Josh Worden <solocommand@gmail.com>
2019-12-16 17:40:36 -0600

    Do not return v1 entries

--- services/graphql/src/mongodb.js
+++ services/graphql/src/mongodb.js
@@ -4,6 +4,7 @@ const { MONGO_DSN, TENANT_KEY } = require('./env');
 const client = new MongoClient(MONGO_DSN);
 const promise = client.connect();
 const tenant = TENANT_KEY;
+const type = { $exists: true };
 
 // eslint-disable-next-line no-console
 promise.then(() => process.stdout.write(`\n💾  MongoDB connected to ${MONGO_DSN}\n`));
@@ -14,6 +15,6 @@ module.exports = {
   retrieve: id => client.db().collection('submission').findOne({ tenant, _id: new ObjectId(id) }),
   insert: payload => client.db().collection('submission').insertOne({ tenant, ...payload }),
   complete: id => client.db().collection('submission').updateOne({ tenant, _id: new ObjectId(id) }, { $set: { reviewed: true } }),
-  submissions: (all = false) => client.db().collection('submission').find({ tenant, ...(!all && { reviewed: false }) }).toArray(),
-  submissionCount: () => client.db().collection('submission').countDocuments({ tenant, reviewed: false }),
+  submissions: (all = false) => client.db().collection('submission').find({ tenant, type, ...(!all && { reviewed: false }) }).toArray(),
+  submissionCount: () => client.db().collection('submission').countDocuments({ tenant, type, reviewed: false }),
 };
