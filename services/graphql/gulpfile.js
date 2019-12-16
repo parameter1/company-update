c31bf3134f6108a389fa9d7378632c78d1515a7e
Josh Worden <solocommand@gmail.com>
2019-12-16 15:02:17 -0600

    fix tests

--- services/graphql/gulpfile.js
+++ services/graphql/gulpfile.js
@@ -1,48 +1,40 @@
-var
-  gulp = require('gulp'),
-  spawn = require('child_process').spawn,
-  node,
-  watchedPaths = [
-    'src/*.js',
-    'src/**/*.js',
-    'src/**/*.graphql',
-  ]
-;
+/* eslint-disable no-console */
+const gulp = require('gulp');
+const { spawn } = require('child_process');
 
-gulp.task('serve', ['lint'], function (cb) {
-  if (node) {
-    node.kill();
-  }
+let node;
+const watchedPaths = ['src/*.js', 'src/**/*.js', 'src/**/*.graphql'];
+
+gulp.task('serve', ['lint'], (cb) => {
+  if (node) node.kill();
 
   node = spawn('node', ['src/index.js'], { stdio: 'inherit' });
 
-  node.on('close', function (code) {
+  node.on('close', (code) => {
     if (code === 8) {
       cb(code);
       console.log('Error detected, waiting for changes...');
     }
   });
   cb();
-})
+});
 
-gulp.task('watch', function () {
-  gulp.watch(watchedPaths, ['serve']);
-})
+gulp.task('watch', () => gulp.watch(watchedPaths, ['serve']));
 
-gulp.task('lint', function (cb) {
-  lint = spawn('./node_modules/.bin/eslint', ['src/**/*.js'], { stdio: 'inherit' });
-  lint.on('close', function (code) {
+gulp.task('lint', (cb) => {
+  const lint = spawn('./node_modules/.bin/eslint', ['src/**/*.js'], { stdio: 'inherit' });
+  lint.on('close', (code) => {
     if (code === 8) {
       cb(code);
       gulp.log('Error detected, waiting for changes...');
     }
     cb();
   });
-})
+});
 
 gulp.task('default', ['watch', 'serve']);
 
 // clean up if an error goes unhandled.
-process.on('exit', function () {
-  if (node) node.kill()
-})
+process.on('exit', () => {
+  if (node) node.kill();
+});
