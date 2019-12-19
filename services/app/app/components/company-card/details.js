90d4ef4bb7462c9dfdf30ac556367915a8331462
Josh Worden <solocommand@gmail.com>
2019-12-19 10:49:08 -0600

    Do not allow paragraph tags in teaser

--- services/app/app/components/company-card/details.js
+++ services/app/app/components/company-card/details.js
@@ -3,12 +3,14 @@ import { htmlSafe } from '@ember/template';
 import { computed } from '@ember/object';
 
 const toolbarButtons = ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript'];
+const htmlAllowedTags = ['a', 'b', 'strong', 'em', 'i', 'u', 's', 'sub', 'sup', 'strike'];
 
 export default Component.extend({
   tagName: 'div',
   classNames: ['card mb-3'],
   title: 'Company Details',
   toolbarButtons,
+  htmlAllowedTags,
   teaser: computed('model.teaser', function() {
     return htmlSafe(this.get('model.teaser'));
   }),
