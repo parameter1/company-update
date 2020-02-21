5f61df16610c98d3fd4705301d278cbdfe7a6e79
Josh Worden <solocommand@gmail.com>
2020-02-21 12:16:28 -0600

    Throw an error instead of logging

--- services/app/app/components/review-card/external-link.js
+++ services/app/app/components/review-card/external-link.js
@@ -1,12 +1,10 @@
 import Component from '@ember/component';
 
-const { log } = console;
-
 export default Component.extend({
   classNames: 'row',
   link: null,
   payload: null,
   original: null,
   updated: null,
-  toggleField: () => log('Pass toggleField!'),
+  toggleField: () => { throw new Error('Pass toggleField!'); },
 });
