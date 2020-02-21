8041261174005968eff9745f9042712aa84feabf
Josh Worden <solocommand@gmail.com>
2020-02-21 12:16:20 -0600

    update computed property and remove actions

--- services/app/app/components/review-card/external-links.js
+++ services/app/app/components/review-card/external-links.js
@@ -4,18 +4,9 @@ import ReviewCard from '@base-cms/company-update-app/components/review-card';
 export default ReviewCard.extend({
   title: 'External Links',
 
-  links: computed('company.externalLinks', 'submission.payload.externalLinks', function() {
+  links: computed('company.externalLinks.[]', 'submission.payload.externalLinks.[]', function() {
     const cl = get(this, 'company.externalLinks') || [];
     const sl = get(this, 'submission.payload.externalLinks') || [];
     return sl.map((v, i) => ({ original: cl[i] ? cl[i] : {}, updated: v }));
   }),
-
-  actions: {
-    add() {
-      this.get('links').pushObject({});
-    },
-    remove(link) {
-      this.get('links').removeObject(link);
-    }
-  },
 });
