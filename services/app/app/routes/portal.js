7cf32344ffe26ae81fa6f9494b2edba0e3d4c1b6
Brian Miller <bmiller@southcomm.com>
2020-09-02 16:15:01 -0500

    Check leadershipCompanyLable in company labels
    Check that leadershipEnable is true && if a leadershipCompay Label is set.
    If so check if the label is in the list of company labels.
    If not set the LeadershipEnable to false.

--- services/app/app/routes/portal.js
+++ services/app/app/routes/portal.js
@@ -1,14 +1,24 @@
 import Route from '@ember/routing/route';
 import { queryManager } from 'ember-apollo-client';
+import { inject } from '@ember/service';
 import query from '@base-cms/company-update-app/gql/queries/portal';
 
 export default Route.extend({
+  config: inject(),
   apollo: queryManager(),
 
   async model({ hash }) {
     const variables = { input: { hash } };
     const model = await this.apollo.query({ query, variables, fetchPolicy: 'network-only' }, 'contentHash');
     if (!model) throw new Error('Invalid URL');
+    
+    // Check that leadershipEnable is true && if a leadershipCompay Label is set.  
+    // If so check if the label is in the list of company labels.
+    // If not set the LeadershipEnable to false.
+    if (this.config.leadershipEnabled && this.config.leadershipCompanyLabel) {
+      if(!model.labels.includes(this.config.leadershipCompanyLabel)) this.config.leadershipEnabled = false;
+    }
+
     return model;
   },
 
