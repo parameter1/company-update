34b78a1ffd7adbb7d9fa437063fd5ee0d6bdcd87
Shinsina <shinsina@att.net>
2021-05-24 14:06:23 -0500

    Removal of passing down of locale config variable.

--- services/app/app/helpers/i18n.js
+++ services/app/app/helpers/i18n.js
@@ -1,14 +1,17 @@
 import translations from './translations';
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
+import { inject } from '@ember/service';
 
-export function i18n ([locale, fieldLabelKey]) {
-  if (!translations[locale]) {
-    throw new Error(`No translations available for requested language ${locale}!`);
+export default Helper.extend({
+  config: inject('config'),
+  compute(fieldLabelKey) {
+    let config = this.get('config');
+    if (!translations[config.locale]) {
+      throw new Error(`No translations available for requested language ${config.locale}!`);
+    }
+    if (!translations[config.locale][fieldLabelKey]) {
+      throw new Error(`No translations available in ${config.locale} for requested key ${fieldLabelKey}!`);
+    }
+    return translations[config.locale][fieldLabelKey];
   }
-  if (!translations[locale][fieldLabelKey]) {
-    throw new Error(`No translations available in ${locale} for requested key ${fieldLabelKey}!`);
-  }
-  return translations[locale][fieldLabelKey];
-}
-
-export default helper(i18n)
+});
