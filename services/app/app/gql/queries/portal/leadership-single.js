044908894f49c4979ed6fc47037b08453a8d8600
Josh Worden <josh@parameter1.com>
2021-12-09 10:23:54 -0600

    Add single site query for leadership categories

---
+++ services/app/app/gql/queries/portal/leadership-single.js
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag';
+import LeadershipSectionFragment from '../../fragments/leadership/section';
+
+export default gql`
+query ContentUpdateLeadershipData(
+  $site: WebsiteSiteQueryInput!,
+  $leaders: WebsiteSectionsQueryInput!,
+  $children: WebsiteSectionChildrenInput!
+) {
+  websiteSite(input: $site) {
+    id
+    name
+  }
+  websiteSections(input: $leaders) {
+    totalCount
+    edges {
+      node {
+        # Primary
+        ...LeadershipSectionFragment
+        children(input: $children) {
+          totalCount
+          edges {
+            node {
+              # Secondary
+              ...LeadershipSectionFragment
+              children(input: $children) {
+                totalCount
+                edges {
+                  node {
+                    # Tertiary
+                    ...LeadershipSectionFragment
+                    children(input: $children) {
+                      totalCount
+                      edges {
+                        node {
+                          # Quaternary
+                          id
+                          name
+                          alias
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+${LeadershipSectionFragment}
+`;
