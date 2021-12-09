03e6407fec69ecf948706a7c78e547c4a3cdff6d
Josh Worden <josh@parameter1.com>
2021-12-09 13:28:06 -0600

    Add support for quinary sections and change layout

--- services/app/app/components/leadership/category.js
+++ services/app/app/components/leadership/category.js
@@ -1,12 +1,19 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { get, computed } from '@ember/object';
 
 export default Component.extend({
-  classNames: ['card-body row'],
+  tagName: 'fieldset',
+  classNames: ['leadership-category', 'border', 'mb-2', 'p-2'],
   category: null,
 
   sections: computed('category.children.edges.[]', function() {
     const sections = this.get('category.children.edges') || [];
     return sections.map(({ node }) => node);
+  }),
+
+  isMaxDepth: computed('sections', function() {
+    const sections = this.get('sections');
+    const hasGrandchildren = sections.some((section) => get(section, 'children.totalCount') > 0);
+    return !hasGrandchildren;
   }),
 });
