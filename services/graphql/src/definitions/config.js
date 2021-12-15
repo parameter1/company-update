b929ce3a0ed6c5c549d3505056a0a9dc438fd7c9
Josh Worden <josh@parameter1.com>
2021-12-15 13:20:21 -0600

    Add leadership category prefix env var

--- services/graphql/src/definitions/config.js
+++ services/graphql/src/definitions/config.js
@@ -15,6 +15,7 @@ module.exports = gql`
     leadershipPromotionsEnabled: Boolean!
     leadershipPrimarySiteOnly: Boolean!
     leadershipCompanyLabel: String
+    leadershipCategoryPrefix: String!
     leadershipSectionAlias: String!
     leadershipSectionMax: Int!
     locale: String!
