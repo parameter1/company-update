47fd4fa9fb8cdd9f55a50492da7798ae51952bc8
Shinsina <shinsina@att.net>
2022-11-02 10:25:07 -0500

    Auto-collapse sub-lists on initial list expansion

--- services/app/app/components/directory/category-tree.js
+++ services/app/app/components/directory/category-tree.js
@@ -1,12 +1,14 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import ActionMixin from '@base-cms/company-update-app/mixins/action';
 
 const { error } = console;
 
-export default Component.extend({
+export default Component.extend(ActionMixin, {
   classNameBindings: ['root:tree-wrapper', 'root:border', 'root:p-2'],
   node: null,
   root: false,
+  collapsed: true,
   onUpdate: () => error('onUpdate missing!'),
 
   rootClass: computed('root', function() {
@@ -20,6 +22,9 @@ export default Component.extend({
   }),
 
   actions: {
+    collapse() {
+      this.set('collapsed', !this.collapsed);
+    },
     update() {
       this.onUpdate(this.node.id);
     },
