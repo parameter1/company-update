334832b28b29401e01067ee0eeaf0e38fc3040f6
Jake Collins <shinsina@att.net>
2023-04-20 15:49:54 -0500

    Update category-tree component controller

--- services/app/app/components/directory/category-tree.js
+++ services/app/app/components/directory/category-tree.js
@@ -1,10 +1,24 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { computed, get } from '@ember/object';
+import { inject } from '@ember/service';
 
 const { error } = console;
 
+const getOwnedIds = (obj, ids = []) => {
+  const edges = obj || [];
+  if (Array.isArray(edges)) {
+    return edges.reduce((arr, node) => [
+      ...arr,
+      node.id,
+      ...(node.children ? getOwnedIds(node.children) : []),
+    ], ids);
+  }
+  return [];
+};
+
 export default Component.extend({
   classNameBindings: ['root:tree-wrapper', 'root:border', 'root:p-2'],
+  config: inject(),
   node: null,
   collapsed: false,
   root: false,
@@ -13,6 +27,13 @@ export default Component.extend({
     this.collapsed = Boolean(this.node.children && this.node.children.length);
   },
   onUpdate: () => error('onUpdate missing!'),
+
+  _selected: computed('selectedIds.[]', 'node.{id,children.[]}', function() {
+    const ids = this.selectedIds || [];
+    const children = get(this, 'node.children') || [];
+    const ownedIds = getOwnedIds(children);
+    return ids.filter(id => ownedIds.includes(id));
+  }),
 
   rootClass: computed('root', function() {
     return this.root ? 'tree-inner' : '';
@@ -23,6 +44,10 @@ export default Component.extend({
     const id = this.node.id;
     return ids.includes(id);
   }),
+
+  selected: computed.reads('_selected.length'),
+
+  maximum: computed.reads('config.directorySelectionMax'),
 
   actions: {
     collapse() {
