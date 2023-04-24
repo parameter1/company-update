3216e477ba1fef7e2a4426a33dca9b858fe718ad
Jake Collins <shinsina@att.net>
2023-04-24 11:51:06 -0500

    Alter router and templating for directory-sections

---
+++ services/app/app/controllers/portal/directory-sections.js
@@ -0,0 +1,36 @@
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import { inject } from '@ember/service';
+
+export default Controller.extend({
+  config: inject(),
+
+  isModalOpen: false,
+  hash: null,
+
+  submitDisabled: computed('categories.[]', function() {
+    return this.get('categories.length') === 0;
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.set('categories', []);
+  },
+
+  actions: {
+    update(id) {
+      const categories = this.get('categories');
+      if (categories.includes(id)) {
+        categories.removeObject(id);
+      } else {
+        categories.pushObject(id);
+      }
+    },
+    showModal() {
+      this.set('isModalOpen', true);
+    },
+    transitionToPortal() {
+      this.transitionToRoute('portal');
+    },
+  }
+});
