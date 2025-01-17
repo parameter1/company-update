ea5a7d0db5e8f1e392a63ba26a44c02c501b1464
Shinsina <shinsina@att.net>
2025-01-17 14:37:14 -0600

    Document card and upload document component controllers

---
+++ services/app/app/components/document-card.js
@@ -0,0 +1,43 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['col-md-6 col-12'],
+  document: null,
+  _document: null,
+
+  init() {
+    this._super(...arguments);
+    const {
+      name,
+      teaser,
+      fileSrc,
+      labels,
+    } = this.get('document');
+    this.set('_document', {
+      name,
+      teaser,
+      ...(fileSrc && { fileSrc } || { fileSrc: null }),
+      ...((Array.isArray(labels) && labels.length) && { labels } || { labels: {} })
+    });
+  },
+
+  actions: {
+    remove() {
+      this.onRemove();
+    },
+    onChange(field) {
+      const original = this.get(`document.${field}`);
+      const updated = this.get(`_document.${field}`);
+      if (original === updated) {
+        this.onReset(field);
+      } else {
+        this.onUpdate(field, updated);
+      }
+    },
+    onUpload(fileSrc) {
+      console.log(fileSrc, 'BOI');
+      this.set('_document.fileSrc', fileSrc);
+      this.onUpdate('fileSrc', fileSrc);
+    }
+  },
+});
---
+++ services/app/app/components/upload-document.js
@@ -0,0 +1,35 @@
+import Component from '@ember/component';
+import ActionMixin from '@base-cms/company-update-app/mixins/action';
+import mutation from '@base-cms/company-update-app/gql/mutations/upload';
+import { inject } from '@ember/service';
+
+const { error } = console;
+
+export default Component.extend(ActionMixin, {
+  apollo: inject(),
+  notify: inject(),
+  document: null,
+
+  actions: {
+    async upload(file) {
+      const document = this.get('document');
+      this.startAction();
+      const variables = { file: file.get('blob') };
+
+      try {
+        const location = await this.apollo.mutate({ mutation, variables }, 'companyUpdateSingleUpload');
+        console.log(location, 'HEYO');
+        this.onUpload(location);
+      } catch (e) {
+        this.set('document', document);
+        error('upload error', e, file);
+        console.log(e);
+        this.notify.error(`Something went wrong -- please try again!<br>${e.message}`, { autoClear: false, htmlContent: true });
+      } finally {
+        file.queue.remove(file);
+        this.endAction();
+      }
+    },
+  },
+
+});
