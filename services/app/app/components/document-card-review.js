473e574b45d6e7ef2912fb511ecfea72b47c6519
Shinsina <shinsina@att.net>
2025-01-31 12:40:38 -0600

    Add controller for document-card-review

---
+++ services/app/app/components/document-card-review.js
@@ -0,0 +1,30 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  classNames: ['col-md-6 col-12'],
+  original: null,
+  updated: null,
+  payload: null,
+
+  canToggleCard: computed.or('isDeleted', 'isNew'),
+  isCardEnabled: computed.reads('payload.enabled'),
+
+  isDeleted: computed('updated', 'original', function() {
+    return this.get('original') && !this.get('updated');
+  }),
+
+  isNew: computed('updated', 'original', function() {
+    console.log(this.get('original'), this.get('updated'), this.get('payload'));
+    return this.get('updated') && !this.get('original');
+  }),
+
+  actions: {
+    toggleCard() {
+      this.set('payload.enabled', !this.get('payload.enabled'));
+    },
+    toggleField(key) {
+      this.set(`payload.fields.${key}`, !this.get(`payload.fields.${key}`));
+    },
+  },
+});
